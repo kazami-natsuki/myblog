8fa2ebcd7966504d6c294380567743531f87fb52
n-kazami <n-kazami@cmind-co.jp>
2020-08-08 14:22:30 +0900

    第3章 - Laravel実践 - 第4部 - コントローラとルーティング (復習)

---
+++ app/Http/Controllers/Admin/WorkController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class WorkController extends Controller
+{
+    public function add()
+    {
+        return view('admin.work.create');
+    }
+
+    public function create()
+    {
+        return redirect('admin/work/create');
+    }
+
+    public function edit()
+    {
+        return view('admin.work.edit');
+    }
+
+    public function update()
+    {
+        return redirect('admin/work/edit');
+    }
+}
