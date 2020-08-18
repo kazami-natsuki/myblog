1ba344ea00b6dbf2072e3584645b0fd0870159e9
n-kazami <n-kazami@cmind-co.jp>
2020-08-18 16:23:42 +0900

    第3章 - Laravel実践 - 第10部 - 投稿データを保存しよう (復習)

--- app/Http/Controllers/Admin/WorkController.php
+++ app/Http/Controllers/Admin/WorkController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Work;
+
 
 class WorkController extends Controller
 {
@@ -14,6 +16,29 @@ class WorkController extends Controller
 
     public function create(Request $request)
     {
+        // Varidationを行う
+      $this->validate($request, Work::$rules);
+
+      $work = new Work;
+      $form = $request->all();
+
+      // フォームから参考資料が送信されてきたら、保存して、$work->file_path に参考資料のパスを保存する
+      if (isset($form['file'])) {
+        $path = $request->file('file')->store('public/file');
+        $work->file_path = basename($path);
+      } else {
+          $work->file_path = null;
+      }
+
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたfileを削除する
+      unset($form['file']);
+
+      // データベースに保存する
+      $work->fill($form);
+      $work->save();
+
         return redirect('admin/work/create');
     }
 
---
+++ database/migrations/2020_08_18_065947_create_works_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name'); // 職歴・会社名を保存するカラム
+            $table->string('description');  // 詳細を保存するカラム
+            $table->string('file_path')->nullable();  // 参考資料を保存するカラム
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works');
+    }
+}
